897ad17b1db548fbde24abde41e96d5107e3c506
huntersteffner <huntersteffner@gmail.com>
2022-08-11 17:38:04 -0400

    User Model. Not sure if it's working

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const {Schema, model} = require('mongoose')
+
+
+// From https://thewebdev.info/2022/03/16/how-to-validate-email-syntax-with-mongoose/
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+  };
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // required: "Email address is required",
+            // validate: [validateEmail, "Please fill a valid email address"],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please fill a valid email address", 
+            ]
+        },
+        thoughts: {
+
+        }
+    },
+    {
+        // I don't know if this is correct, but I took from Act 23 from Unit 18
+        toJSON: {
+            virtuals: true,
+          },
+          id: false,
+    }
+)
+
+const User = model('user', userSchema)
+
+module.exports = User
